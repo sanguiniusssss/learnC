88b0aea290ebf542d703c10dc0d16df927d77640
sanguiniusssss <yang11429@outlook.com>
2025-10-21 14:34:56 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,9 +1,14 @@
-//
-//
-//
-#include <stdio.h>
+//202510125115
+//yang11429@outlook.com
+//杨润
+#include<stdio.h>
 int main()
 {
-    printf("Hello World");
+    float fah=0.0;
+    float cen=0.0;
+    scanf("%f",&cen);
+    fah=(cen*9.0/5.0)+32.0;
+    printf("%.1f",fah);
+
     return 0;
 }
