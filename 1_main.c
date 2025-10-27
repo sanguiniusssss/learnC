155e6ee0bfc08bfd194c2f1cadcbed369cee3039
sanguiniusssss <yang11429@outlook.com>
2025-10-27 11:16:36 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -4,11 +4,28 @@
 #include<stdio.h>
 int main()
 {
-    float fah=0.0;
-    float cen=0.0;
-    scanf("%f",&cen);
-    fah=(cen*9.0/5.0)+32.0;
-    printf("%.1f",fah);
-
-    return 0;
+	int n = 0;
+	scanf("%d", &n);
+	int i = 0;
+	int isPrime = 1;
+	if(n<=50)
+	{ 
+		for (i = 2; i < 25; i++)
+		{
+			if (n % i == 0)
+			{
+				isPrime = 0;
+				break;
+			}
+		}
+		if (isPrime == 1)
+		{
+			printf("密钥安全，密码设置成功\n");
+		}
+		else
+		{
+			printf("密钥不安全，请重新输入");
+		}
+	}
+	return 0;
 }
