f4364a5425486da75096026bd372ba55abdde83e
sanguiniusssss <yang11429@outlook.com>
2025-10-27 11:34:01 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -2,12 +2,23 @@
 //yang11429@outlook.com
 //杨润
 #include <stdio.h>
-int main() 
-{
-    float celsius;  
-    float fahrenheit;
-    scanf("%f", &celsius);
-    fahrenheit = celsius * 9.0 / 5.0 + 32;
-    printf("%.1f\n", fahrenheit);
-    return 0;
+int main() {
+	int n = 0;
+	int a = 0;
+	int isSafe = 0;
+	scanf("%d", &n);
+	for (a = 1; a <= 50; a++) {
+		if (n % a == 0) 
+		{ 
+			isSafe = 1;
+			break;
+		}
+	}
+	if (isSafe) {
+		printf("密钥安全，密码设置成功\n");
+	}
+	else {
+		printf("密钥不安全，请重新输入\n");
+	}
+	return 0;
 }
