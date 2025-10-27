98967990cc7fe85c9370a0d47559dbb9667c4876
sanguiniusssss <yang11429@outlook.com>
2025-10-27 11:36:30 +0800

    Update 2_test.c

--- 2_test.c
+++ 2_test.c
@@ -1,30 +1,29 @@
 //202510125115
 //yang11429@outlook.com
 //杨润
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int score=0;
-    scanf("%d",&score);
-    if(score>=90&&score<=100)
-    {
-        printf("A\n");
-    }
-    else if(score>=80)
-    {
-        printf("B\n");
-    }
-    else if(score>=70)
-    {
-        printf("C\n");
-    }
-    else if(score>=60)
-    {
-        printf("D\n");
-    }
-    else
-    {
-        printf("E\n");
-    }
-    return 0;
+	int n = 0;
+	int j = 0;
+	int i = 0;
+	int k = 0;
+	int a = 0;
+	for (n = 100; n <= 999; n++)
+	{
+		j = n / 100;
+		k = n % 100 / 10;
+		i = n %10;
+
+		if (j* j* j + k * k * k + i * i * i ==n)
+		{
+			if (a == 1)
+			{
+				printf(" ");
+			}
+			printf("%d", n);
+			a = 1;
+		}
+	}
+	return 0;
 }
