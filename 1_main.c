ea6e20617590126eba24630130ef6b71b4fcb849
sanguiniusssss <yang11429@outlook.com>
2025-10-27 11:22:21 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,31 +1,13 @@
 //202510125115
 //yang11429@outlook.com
 //杨润
-#include<stdio.h>
-int main()
-{
-	int n = 0;
-	scanf("%d", &n);
-	int i = 0;
-	int isPrime = 1;
-	if(n<=50)
-	{ 
-		for (i = 2; i < 25; i++)
-		{
-			if (n % i == 0)
-			{
-				isPrime = 0;
-				break;
-			}
-		}
-		if (isPrime == 1)
-		{
-			printf("密钥安全，密码设置成功\n");
-		}
-		else
-		{
-			printf("密钥不安全，请重新输入");
-		}
-	}
-	return 0;
+#include <stdio.h>
+
+int main(void) {
+    float celsius;  
+    float fahrenheit;
+    scanf("%f", &celsius);
+    fahrenheit = celsius * 9.0 / 5.0 + 32;
+    printf(" %.1f\n", fahrenheit);
+    return 0;
 }
