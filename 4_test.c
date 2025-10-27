12443122c06915192b3936779d07e63a1847ad4b
sanguiniusssss <yang11429@outlook.com>
2025-10-27 11:42:30 +0800

    Update 4_test.c

--- 4_test.c
+++ 4_test.c
@@ -4,17 +4,27 @@
 #include<stdio.h>
 int main()
 {
-    int a=0;
-    int b=0;
-    int c=0;
-    scanf("%d %d %d",&a,&b,&c);
-    if(a+b>c&&a+c>b&&b+c>a)
-    {
-        printf("可以组成三角形");
-    }
-    else
-    {
-        printf("不能组成三角形");
-    }
-    return 0;
+	int n = 100;
+	int j = 0;
+	int i = 0;
+	int k = 0;
+	int a = 0;
+	while(n <= 999)
+	{
+		j = n / 100;
+		k = n % 100 / 10;
+		i = n % 10;
+
+		if (j * j * j + k * k * k + i * i * i == n)
+		{
+			if (a == 1)
+			{
+				printf(" ");
+			}
+			printf("%d", n);
+			a = 1;
+		}
+		n++;
+	}
+	return 0;
 }
