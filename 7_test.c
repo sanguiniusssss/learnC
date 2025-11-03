7e05397232946491204ec8cf056b5cce3d0381ee
sanguiniusssss <yang11429@outlook.com>
2025-11-03 18:59:31 +0800

    Update 7_test.c

--- 7_test.c
+++ 7_test.c
@@ -2,27 +2,36 @@
 //yang11429@outlook.com
 //杨润
 #include<stdio.h>
+void Return(int arr[], int length)
+{
+
+	int temp = 0;
+	int j = 0;
+
+	for (j = 0; j < length / 2; j++)
+	{
+		temp = arr[j];
+		arr[j] = arr[length - 1 - j];
+		arr[length - 1 - j] = temp;
+	}
+}
 int main()
 {
+	int arr[5] = { 0 };
 	int i = 0;
-	int num[5] = { 0 };
 	int a = 0;
-	for (i = 0; i <4; i++)
+	for (i = 0; i < 5; i++)
 	{
-		scanf("%d", &num[i]);
+		scanf("%d", &arr[i]);
 	}
-	for (i = 4; i >0; i--)
-	{
-		num[i] = num[i-1];
-	}
-	num[0] = 0;
+	Return(arr,5);
 	for (i = 0; i < 5; i++)
 	{
 		if (a == 1)
 		{
 			printf(" ");
 		}
-		printf("%d", num[i]);
+		printf("%d", arr[i]);
 		a = 1;
 	}
 	return 0;
