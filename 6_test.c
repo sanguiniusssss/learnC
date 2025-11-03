69bfcf09a73801bea7ac0071cb52508c2fdb8f6e
sanguiniusssss <yang11429@outlook.com>
2025-11-03 18:58:44 +0800

    Update 6_test.c

--- 6_test.c
+++ 6_test.c
@@ -2,31 +2,37 @@
 //yang11429@outlook.com
 //杨润
 #include<stdio.h>
+int sum(int arr[],int length)
+{
+	int Sum = 0;
+	int i = 0;
+	for (i = 0; i < length; i++)
+	{
+		Sum += arr[i];
+	}
+	return Sum;
+}
+int product(int arr[], int length)
+{
+	int Product = 1;
+	int i = 0;
+	for (i = 0; i < length; i++)
+	{
+		Product *= arr[i];
+	}
+	return Product;
+}
 int main()
 {
-	int i = 0;
-	int j = 0;
-	int a = 0;
-	int k = 0;
 	int arr[5] = {0};
-	while(i<5)
+	int i = 0;
+	for (i = 0; i < 5; i++)
 	{
-		scanf("%d",&j);
-		if (j % 2 == 0)
-		{
-			arr[i] = j;
-			i++;
-		}
-		
+		scanf("%d", &arr[i]);
 	}
-	for (k = 0; k < 5; k++)
-	{
-		if (a == 1)
-		{
-			printf(" ");
-		}
-		printf("%d", arr[k]);
-		a = 1;
-	}
+	int a = sum(arr,5);
+	int b = product(arr,5);
+	printf("%d %d", a, b);
+	
 	return 0;
 }
