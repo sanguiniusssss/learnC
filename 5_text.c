bc932c66e7bce0fe7d7873a6b2ad0eea2762fb60
sanguiniusssss <yang11429@outlook.com>
2025-11-16 20:20:11 +0800

    Update 5_text.c

--- 5_text.c
+++ 5_text.c
@@ -2,19 +2,33 @@
 //yang11429@outlook.com
 //杨润
 #include<stdio.h>
-int sum(int a1, int an,int step)
+int push(int ptr_arr[], int len)
 {
 	int i = 0;
-	int result = 0;
-	for (i = a1; i <= an; i = i + step)
+	int* p = ptr_arr;
+
+	for (i = len-1; i >0 ; i--)
 	{
-		result += i;
+		*(p + i) = *(p + i - 1);
 	}
-	return result;
+	*p = 0;
 }
 int main()
 {
-	int a = sum(1,100,1);
-	printf("%d", a);
+	int arr[5] = { 0 };
+	int i = 0;
+	int* p = arr;
+	for (i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
+	{
+		scanf("%d", p);
+		p++;
+	}
+	push(arr, 5);
+	p = arr;
+	for (i = 0; i < 5; i++)
+	{
+		printf("%d ", *p);
+		p++;
+	}
 	return 0;
 }
