e29dc8c319896ff9851452c2e6f9a95174dfef61
sanguiniusssss <yang11429@outlook.com>
2025-11-16 20:19:10 +0800

    Update 3_test.c

--- 3_test.c
+++ 3_test.c
@@ -2,31 +2,21 @@
 //yang11429@outlook.com
 //杨润
 #include<stdio.h>
+void swap(int* a,int*b)
+{
+	int temp = 0;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+
+}
 int main()
 {
-	int arr[10] = {0};
-	int i = 0;
-	int j = 0;
-	for (i = 0; i < 10; i++)
-	{
-		scanf("%d", &arr[i]);
-	}
-	for (int i = 0; i < 10 - 1; i++)
-	{
-		for (int j = 0; j < 10 - i - 1; j++)
-		{
-			if (arr[j] > arr[j + 1])
-			{
-				int temp = arr[j];
-				arr[j] = arr[j + 1];
-				arr[j + 1] = temp;
-			}
+	int x = 0;
+	int y = 0;
+	scanf("%d %d", &x,&y);
+	swap(&x,&y);
+	printf("%d %d", x,y);
 
-		}
-	}
-	for (i = 0; i < 10; i++)
-	{
-		printf("%d ", arr[i]);
-	}
 	return 0;
 }
