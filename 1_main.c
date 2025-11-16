717ee2c2d295d1b251d0896459d12c129fb09d71
sanguiniusssss <yang11429@outlook.com>
2025-11-16 20:07:38 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -4,23 +4,10 @@
 #include<stdio.h>
 int main()
 {
-	int arr[3][3] = { 0 }；
-	int i = 0;
-	int j = 0;
-	for (i = 0; i < 3; i++)
-	{
-		for (j = 0; j < 3; j++)
-		{
-			scanf("%d",&arr[i][j]);
-		}
-	}
-	for (i = 0; i < 3; i++)
-	{
-		for (j = 0; j < 3; j++)
-		{
-			printf("%d ", arr[i][j]);
-		}
-		printf("\n");
-	}
+	int a = 0;
+	scanf("%d",&a);
+	int* p = &a;
+	*p += 10;
+	printf("%d,%d", a, *p);
 	return 0;
 }
